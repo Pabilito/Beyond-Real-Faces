8bc8616cd5d83f33a22387963454086c67b5ca3a
Pawel BORSUKIEWICZ <pawel.borsukiewicz@uni.lu>
2025-06-26 15:05:20 +0200

    check whole datasets

--- CASIA_Compare/find_closest.sh
+++ CASIA_Compare/find_closest.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 input_folder="Embeddings"
-n_comparisons=10000
-
 for file in "$input_folder"/*.json; do
     [[ "$(basename "$file")" == "CASIA.json" ]] && continue
     echo "Processing: $file"
-    python compare_embeddings.py "$file" --n_comparisons "$n_comparisons"
+    # Count number of lines in the JSON file
+    n_comparisons=$(wc -l < "$file")
+    python compare_embeddings.py "$file" --n_comparisons "$n_comparisons" --casia_file "$input_folder"/CASIA.json
 done
